bd180b934ff9180dba14cc910777b708a229d7d2
taeyangcode <taeyangcode@gmail.com>
2023-12-04 20:38:59 -0800

    cs 16: extra credit comments added

--- extra_credit.asm
+++ extra_credit.asm
@@ -17,14 +17,19 @@ INCLUDE Irvine32.inc
 
 .code
 
+; function that draws a row of a chessboard beginning with white and then color
+; accepts a variable color parameter to represent the colored squares
 white_color_row PROTO,
 	p_x: BYTE,
 	p_color: DWORD,
 
+; function that draws a row of a chessboard beginning with color and then white
+; accepts a variable color parameter to represent the colored squares
 color_white_row PROTO,
 	p_x: BYTE,
 	p_color: DWORD,
 
+; draw a complete chess board accepting a parameter color for the colored squares
 draw_board PROTO,
 	p_color: DWORD
 
@@ -32,53 +37,49 @@ white_color_row PROC USES eax edx,
 	p_x: BYTE,
 	p_color: DWORD
 
-	mov dh, p_x
-	mov dl, 0
-
-	mov ecx, 4
+	mov dh, p_x			; draw row starting from the x coordinate parameter
+	mov dl, 0			; begin drawing squares from y coordinate 0
 
 DRAW_ROW:
-	call Gotoxy
+	call Gotoxy			; begin by moving to current position
 
-	mov eax, white * 16
-	call SetTextColor
+	mov eax, white * 16		; change background color to white
+	call SetTextColor		; set background color to white
 
-	mov al, ' '
-	call WriteChar
+	mov al, ' '			; move space character to write onto screen
+	call WriteChar			; draw space character
 
-	inc dl
+	inc dl				; increment y coordinate to draw next square
 
-	call Gotoxy
+	call Gotoxy			; move to next position
 
-	mov eax, p_color
-	shl eax, 4
-	call SetTextColor
+	mov eax, p_color		; change background color to specified color parameter
+	shl eax, 4			; multiply by 16 to get background color value
+	call SetTextColor		; set background color to parameter
 
-	mov al, ' '
-	call WriteChar
+	mov al, ' '			; move space character to write
+	call WriteChar			; draw space character
 
-	inc dl
+	inc dl				; increment y coordinate to draw next square
 
-	cmp dl, 7
+	cmp dl, 7			; if 8 squares have been draw -> exit function
 	jae EXIT_FUNCTION
 
-	jmp DRAW_ROW
+	jmp DRAW_ROW			; else -> loop
 
 EXIT_FUNCTION:
-	mov eax, black
+	mov eax, black			; change text color to black to prevent drawing more characters to terminal
 	call SetTextColor
 
 	ret
 white_color_row ENDP
 
-color_white_row PROC USES eax edx,
+color_white_row PROC USES eax edx,	; function uses same logic as white_color_row with only the drawing order swapped
 	p_x: BYTE,
 	p_color: DWORD
 	
 	mov dh, p_x
 	mov dl, 0
-
-	mov ecx, 4
 
 DRAW_ROW:
 	call Gotoxy
@@ -118,44 +119,44 @@ color_white_row ENDP
 draw_board PROC USES eax,
 	p_color: DWORD
 
-	mov eax, 0
+	mov eax, 0					; initialize row counter
 
 DRAW_ROWS:
-	INVOKE white_color_row, al, p_color
-	inc eax
+	INVOKE white_color_row, al, p_color		; draw white/color row using parameter color
+	inc eax						; increment row counter
 
-	INVOKE color_white_row, al, p_color
-	inc eax
+	INVOKE color_white_row, al, p_color		; draw color/whtie row using parameter color
+	inc eax						; increment row counter
 
-	cmp eax, 7
+	cmp eax, 7					; if 8 rows have been draw -> exit function
 	jae EXIT_FUNCTION
 
-	jmp DRAW_ROWS
+	jmp DRAW_ROWS					; else -> draw more rows
 
 EXIT_FUNCTION:
 	ret
 draw_board ENDP
 
 part_01 PROC
-	INVOKE draw_board, gray
+	INVOKE draw_board, gray		; draw gray chessboard
 
 	ret
 part_01 ENDP
 
 part_02 PROC USES eax ebx
-	mov ebx, 0
+	mov ebx, 0			; initialize color tracker
 
 DRAW_BOARDS:
-	INVOKE draw_board, ebx
-	inc ebx
+	INVOKE draw_board, ebx		; draw chessboard with current color
+	inc ebx				; increment color tracker
 
-	mov eax, 500
+	mov eax, 500			; 500 millisecond delay
 	call Delay
 
-	cmp ebx, 15
+	cmp ebx, 15			; if 16 colors have been displayed -> exit function
 	jae EXIT_FUNCTION
 
-	jmp DRAW_BOARDS
+	jmp DRAW_BOARDS			; else -> draw next chessboard
 	
 EXIT_FUNCTION:
 	ret
@@ -163,7 +164,7 @@ part_02 ENDP
 
 main PROC
 	; call part_01
-	call part_02
+	; call part_02
 
 	INVOKE ExitProcess, 1
 main ENDP
